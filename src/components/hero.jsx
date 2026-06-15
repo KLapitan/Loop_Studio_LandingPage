901f7fd95a0414a79ac0ec416df005f3a747b3a9
KLapitan <karllapitan5@gmail.com>
2026-06-15 17:08:25 +0800

    tobecon : title

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -13,8 +13,10 @@ return (
 
       {/* background  */}
         <div className="bg-[url('/images/mobile/image-hero.jpg')] sm:bg-[url('/images/desktop/image-hero.jpg')]  bg-cover  bg-no-repeat bg-center h-screen flex justify-start  items-center px-4">
+
         
-            <div className="border-2 border-PWhite h-auto max-w-6xl w-full lg:w-sm lg:ml-47">
+        
+            <div className="border-2 border-PWhite h-auto max-w-6xl w-full   lg:w-sm lg:ml-32 2xl:ml-51">
               <p className="text-PWhite text-4xl lg:text-5xl lg:w-sm lg:leading-12 font-Josefin-Sans p-4 leading-9">IMMERSIVE EXPERIENCES THAT DELIVER</p>
 
             </div>
@@ -30,3 +32,6 @@ return (
 
 )}
 export default LPHero
+
+
+// todo : hero title redo or need to go insideof another div?
