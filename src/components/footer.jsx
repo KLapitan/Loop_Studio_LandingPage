839a840886e41c727eaa28df226a2ae799a23aaf
KLapitan <karllapitan5@gmail.com>
2026-06-17 16:46:38 +0800

    added attribution for finish code

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,8 +9,15 @@ return(
 <footer className="w-full h-auto bg-PBlack flex items-center justify-center ">
     <section className="w-full max-w-6xl h-auto sm:h-49 flex flex-col md:flex-row md:justify-between  gap-9 py-10 sm:py-6 mb-2 sm:mb-0">
     <div className=" flex flex-col  items-center gap-8 md:gap-5 md:justify-center md:items-start  flex-1  md:p-4 lg:p-2">
+       <div className="flex flex-col sm:flex-row text-sm">
+   <span className="text-white ">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="underline underline-offset-4 mr-2">Frontend Mentor.</a> </span> 
+   <span className="text-white">Coded by <a href="https://github.com/KLapitan" className="underline underline-offset-4">Karl Lapitan</a>.</span> 
+  </div>
+
+
+
         <picture>
-            <img src="/logo.svg" alt="logo-loopstudio" className="mt-8 sm:mt-7"/>
+            <img src="/logo.svg" alt="logo-loopstudio" className=" sm:mt-7"/>
         </picture>
 
       <ul className="flex  flex-col md:flex-row gap-7 lg:gap-10 mb-3 ">
@@ -26,6 +33,8 @@ return(
 
     </div>
     <div className="flex  flex-col gap-4 items-center md:items-end md:gap-3 md:justify-center  px-4 ">
+       
+
         <ul className="flex flex-row gap-4 lg:gap-5 p-4">
         {loopSocialLinks.map((link)  => (
         <li key={link.id} className="cursor-pointer group inline-block relative ">
